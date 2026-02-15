6fcb1c0f177858f098333a57ee43146f90787428
Gastrochef83 <mohamad.under500@gmail.com>
2026-02-15 19:58:19 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
-import { ModeProvider } from './state/mode'
+import { ModeProvider } from './lib/mode'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
