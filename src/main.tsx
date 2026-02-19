e8a84e8931a04e1e419967cdd0403adb172b0361
Gastrochef83 <mohamad.under500@gmail.com>
2026-02-19 14:18:04 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 
-// ✅ ONLY this
+// ✅ styles must be imported here
 import './styles.css'
 
 import { ModeProvider } from './lib/mode'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ModeProvider>
-      <App />
-    </ModeProvider>
+    <HashRouter>
+      <ModeProvider>
+        <App />
+      </ModeProvider>
+    </HashRouter>
   </React.StrictMode>
 )
