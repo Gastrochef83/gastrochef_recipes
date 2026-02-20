1fd983d220e3c80b361e8038e2c5690198fab035
Gastrochef83 <mohamad.under500@gmail.com>
2026-02-20 16:44:34 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,28 @@ import Recipes from './pages/Recipes'
 import RecipeEditor from './pages/RecipeEditor'
 import Settings from './pages/Settings'
 
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 export default function App() {
   return (
     <Routes>
+      {/* Auth pages */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      {/* App shell */}
       <Route path="/" element={<AppLayout />}>
-
         <Route index element={<Navigate to="dashboard" replace />} />
-
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="ingredients" element={<Ingredients />} />
         <Route path="recipes" element={<Recipes />} />
         <Route path="recipe" element={<RecipeEditor />} />
         <Route path="settings" element={<Settings />} />
-
       </Route>
 
       {/* fallback */}
       <Route path="*" element={<Navigate to="/dashboard" replace />} />
-
     </Routes>
   )
 }
