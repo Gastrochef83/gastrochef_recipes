889b4cd7667ce66c57128a3c9f4c6074041f5c5a
Gastrochef83 <mohamad.under500@gmail.com>
2026-02-21 14:42:19 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Dashboard from './pages/Dashboard'
 import Ingredients from './pages/Ingredients'
 import Recipes from './pages/Recipes'
 import RecipeEditor from './pages/RecipeEditor'
+import RecipeCookMode from './pages/RecipeCookMode'
 import Settings from './pages/Settings'
 import Login from './pages/Login'
 import Register from './pages/Register'
@@ -24,6 +25,8 @@ export default function App() {
         <Route path="ingredients" element={<Ingredients />} />
         <Route path="recipes" element={<Recipes />} />
         <Route path="recipe" element={<RecipeEditor />} />
+        {/* Cook mode is opened from RecipeEditor via /cook?id=... */}
+        <Route path="cook" element={<RecipeCookMode />} />
         <Route path="settings" element={<Settings />} />
 
       </Route>
