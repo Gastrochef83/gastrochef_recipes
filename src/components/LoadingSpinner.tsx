46bc15462ee32411cc58d740565feee7aeaafbe4
Gastrochef83 <mohamad.under500@gmail.com>
2026-03-16 23:44:53 +0300

    Delete

--- src/components/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function LoadingSpinner() {
-  return (
-    <div className="gc-loading">
-      <div className="gc-spinner" aria-label="Loading" />
-    </div>
-  )
-}
