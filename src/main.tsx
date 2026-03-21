43494da5320a7bb0b41e2fd2094c24a48ad9a3d2
Gastrochef83 <mohamad.under500@gmail.com>
2026-03-21 15:04:32 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,12 @@
+// src/main.tsx
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
 import App from './App'
-
-import './index.css'
-import './styles.css'
-
-import { ModeProvider } from './lib/mode'
-import { AutosaveProvider } from './contexts/AutosaveContext'
-import ErrorBoundary from './components/ErrorBoundary'
-
-function removePreloadSplash() {
-  const el = document.getElementById('gc-preload')
-  if (!el) return
-  // remove after first paint to avoid white flash
-  requestAnimationFrame(() => {
-    requestAnimationFrame(() => el.remove())
-  })
-}
+import './index.css'        // Tailwind - موجود
+import './styles/neo-header.css'  // أنماط الـ Header الجديدة - أضف هذا
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <HashRouter>
-    <ModeProvider>
-      <ErrorBoundary>
-        <AutosaveProvider>
-          <App />
-        </AutosaveProvider>
-      </ErrorBoundary>
-    </ModeProvider>
-  </HashRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
-
-removePreloadSplash()
